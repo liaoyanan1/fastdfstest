d557af9c88ba96b01b48643427d163b976743ad1
liaoyanan1 <1304418038@qq.com>
2020-01-13 09:55:21 +0800

    fdfs工具类

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import com.example.utils.FdfsUtil;
 import org.csource.common.MyException;
-import org.csource.fastdfs.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
@@ -15,26 +16,21 @@ public class TestController {
     public String test(){
         return "index";
     }
+
     @RequestMapping("/test1")
     @ResponseBody
     public String test1(MultipartFile file) throws IOException, MyException {
-        System.out.println(1);
-        ClientGlobal.initByProperties("fastdfs-client.properties");
-        System.out.println("network_timeout="+ClientGlobal.g_network_timeout+"ms");
-        System.out.println("charset="+ClientGlobal.g_charset);
-        TrackerClient tc = new TrackerClient();
-        TrackerServer ts = tc.getTrackerServer();
-        if (ts==null){
-            System.out.println("getConnection return null");
-            return "-1";
-        }
-        StorageServer storeStorage = tc.getStoreStorage(ts);
-        if(storeStorage==null){
-            System.out.println("getStoreStorage return null");
-            return "-1";
-        }
-        StorageClient1 storageClient1 = new StorageClient1(ts,storeStorage);
-        String path = storageClient1.upload_appender_file1(file.getBytes(),".txt",null);
-        return path;
+        return FdfsUtil.upLoad(file);
+    }
+
+    @RequestMapping("/test2")
+    public void test2(String path, HttpServletResponse response) throws IOException, MyException {
+        byte[] bytes = FdfsUtil.downLoad(path);
+        response.getOutputStream().write(bytes);
+    }
+
+    @RequestMapping("/test3")
+    public String test3(){
+        return "test";
     }
 }
